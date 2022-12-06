b10b4a08d71076f3eaeb5571fe78e9e92d7a1edd
Frontend_boantek <fabricioreynacordes@gmail.com>
2022-12-06 15:13:21 -0300

    Reciente

--- j/app.js
+++ j/app.js
@@ -41,3 +41,55 @@ window.onscroll = function(){
 
 const descargar = document.getElementById("descargar")
 
+
+
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("correo")
+const tel = document.getElementById("telefono")
+const form = document.getElementById("formulario")
+const parrafo = document.getElementById("warnings")
+const tipo = document.getElementById("tema")
+
+
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+	let warnings = ""
+	let regexusuario = /^[a-zA-Z0-9\_\-]{4,16}$/;
+	let regexemail =  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+	let regextel = /^\(?\d{2}\)?[\s\.-]?\d{4}[\s\.-]?\d{4}$/
+	parrafo.innerHTML=""
+	if(!regexusuario.test(nombre.value)){
+		warnings += `Nombre no introducido <br>`
+		entrar = true
+	} else if (nombre.value.length < 8){
+		warnings +=`<br> No Introduciste mas de 8 caracteres <br>`
+		entrar = true
+	}
+	if(!regexemail.test(email.value)){
+		warnings += `El email no es valido <br>`
+		entrar = true
+	}
+	if(!regextel.test(tel.value)){
+		warnings += `El numero celular no es valido <br>`
+		entrar = true
+
+	} 
+    if(!regexusuario.test(tipo.value)){
+        warnings += `Introduce un tema <br>` 
+		entrar = true
+
+    }
+	if(entrar){
+		parrafo.innerHTML = warnings
+	}else{
+		parrafo.innerHTML = "Enviado"
+	}
+}
+)
